49cd840ac5e0bf5005c7b8bcf2621b0637c23aac
Ahmad-Hiary <233337377+Ahmad-Hiary@users.noreply.github.com>
2026-01-10 13:33:33 +0300

    Is date1 after date2 .

---
+++ Problem #56.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+short ReadNumber(string Message)
+{
+    cout << Message;
+    short n;
+    cin >> n;
+
+    return n;
+}
+
+struct strDate
+{
+    short Year, Month, Day;
+};
+
+bool IsDate1BeforeDate2(strDate Date1, strDate Date2)
+{
+    if (Date1.Year != Date2.Year)
+        return Date1.Year < Date2.Year;
+    else if (Date1.Month != Date2.Month)
+        return Date1.Month < Date2.Month;
+    else
+        return Date1.Day < Date2.Day;
+
+    //return (Date1.Year < Date2.Year) ? true : ((Date1.Year == Date2.Year) ? (Date1.Month < Date2.Month ? true : (Date1.Month == Date2.Month ? Date1.Day < Date2.Day : false)) : false);
+
+}
+
+bool IsDate1EqualDate2(strDate Date1, strDate Date2)
+{
+    return (Date1.Year == Date2.Year) ? ((Date1.Month == Date2.Month) ? ((Date1.Day == Date2.Day) ? true : false) : false) : false;
+}
+
+bool IsDate1AfterDate2(strDate Date1, strDate Date2)
+{
+    return (!IsDate1BeforeDate2(Date1, Date2) && (!IsDate1EqualDate2(Date1, Date2)));
+}
+
+strDate ReadDate()
+{
+    strDate Date;
+
+    Date.Year = ReadNumber("Please enter year : ");
+    Date.Month = ReadNumber("\n\nPlease enter Month : ");
+    Date.Day = ReadNumber("\n\nPlease enter Day : ");
+
+
+    return Date;
+}
+
+int main()
+{
+
+    strDate Date1 = ReadDate();
+
+    cout << "\n\n";
+
+    strDate Date2 = ReadDate();
+
+    if (IsDate1AfterDate2(Date1, Date2))
+        cout << "\nYes Date 1 After date 2 (:\n\n";
+    else
+        cout << "\No Date 1 not After date 2 (:\n\n";
+
+    system("pause>0");
+}
